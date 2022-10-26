fe7971201a2db0a7c68dca1c083d1b9c0d1868d6
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-10-26 16:09:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ locals {
       rule.protocol,
       rule.from_port,
       rule.to_port,
+      source_security_group_id = lookup(each.value,"source_security_group_id", null),
       try(rule["description"], null) == null ? md5(format("Managed by Terraform #%d", indx)) : md5(rule.description)
     ) => rule
   } : {}
@@ -30,7 +31,7 @@ resource "aws_security_group_rule" "this" {
   protocol          = each.value.protocol
   description       = lookup(each.value, "description", "Managed by Terraform")
   cidr_blocks      = try(length(lookup(each.value, "cidr_blocks", [])), 0) > 0 ? each.value["cidr_blocks"] : null
-  source_security_group_id = var.source_security_group_id
+  source_security_group_id = each.value.source_security_group_id
   
 
 
