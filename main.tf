e4ab96ba25c1575a8d9726922ba3d089400053a2
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-10-26 16:29:04 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,15 @@ resource "aws_security_group_rule" "this" {
   cidr_blocks      = try(length(lookup(each.value, "cidr_blocks", [])), 0) > 0 ? each.value["cidr_blocks"] : null
   source_security_group_id = each.value.source_security_group_id
   
-
-
+}
+resource "aws_security_group_rule" "this1" {
+  for_each = local.rules
+  security_group_id = aws_security_group.this.*.id[0]
+  type              = each.value.type
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  description       = lookup(each.value, "description", "Managed by Terraform")
+  cidr_blocks      = try(length(lookup(each.value, "source_security_group_id", [])), 0) > 0 ? each.value["source_security_group_id"] : null
+  
 }
