22ec87124e7c0f923a898e844ec67a85ec9bbf19
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-10-26 16:41:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,17 +11,7 @@ locals {
       try(rule["description"], null) == null ? md5(format("Managed by Terraform #%d", indx)) : md5(rule.description)
     ) => rule
   } : {}
-  
-  ruless = var.rules != null ? {
-    for indx, rule in flatten(var.rules) :
-    format("%v-%v-%v-%v-%s",
-      rule.type,
-      rule.protocol,
-      rule.from_port,
-      rule.to_port,
-      try(rule["description"], null) == null ? md5(format("Managed by Terraform #%d", indx)) : md5(rule.description)
-    ) => rule
-  } : {}
+ 
 }
 
 resource "aws_security_group" "this" {
@@ -40,18 +30,8 @@ resource "aws_security_group_rule" "this" {
   to_port           = each.value.to_port
   protocol          = each.value.protocol
   description       = lookup(each.value, "description", "Managed by Terraform")
-  cidr_blocks      = try(length(lookup(each.value, "cidr_blocks", [])), 0) > 0 ? each.value["cidr_blocks"] : null
+  source_security_group_id      = try(length(lookup(each.value, "source_security_group_id", [])), 0) > 0 ? each.value["source_security_group_id"] : null
  
   
 }
-resource "aws_security_group_rule" "this1" {
-  for_each = local.ruless
-  security_group_id = aws_security_group.this.*.id[0]
-  type              = each.value.type
-  from_port         = each.value.from_port
-  to_port           = each.value.to_port
-  protocol          = each.value.protocol
-  description       = lookup(each.value, "description", "Managed by Terraform")
-  source_security_group_id      = try(length(lookup(each.value, "source_security_group_id", [])), 0) > 0 ? each.value["source_security_group_id"] : null
-  
-}
+
